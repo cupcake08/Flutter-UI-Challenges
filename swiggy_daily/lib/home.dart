61b5ebba815ad861e44ed78fd7327cd36ffc6213
cupcake08 <bhankhariaa@gmail.com>
2023-05-11 15:52:37 +0530

    :tada: swiggy daily project stuff

--- swiggy_daily/lib/home.dart
+++ swiggy_daily/lib/home.dart
@@ -1,5 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as dev show log;
+
+import 'package:swiggy_daily/data.dart';
 
 extension Log on Object {
   void log() {
@@ -15,9 +18,84 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+  late final List<Widget> _items;
+
+  late final ValueNotifier<int> _tabNotifier;
+
+  final _initialTab = 1;
+
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController.addListener(_listener);
+    _tabNotifier = ValueNotifier(_initialTab);
+    _items = List.generate(
+      4,
+      (index) => Text(
+        _getText(index + 1),
+        overflow: TextOverflow.ellipsis,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 30,
+        ),
+      ),
+    );
+  }
+
+  _listener() {
+    _tabNotifier.value = _tabController.index;
+  }
+
+  _getText(int index) {
+    String text = '';
+    switch (index) {
+      case 1:
+        text = 'BreakFast';
+      case 2:
+        text = 'Lunch';
+      case 3:
+        text = 'Snacks';
+      case 4:
+        text = 'Dinner';
+    }
+    return text;
+  }
+
+  Widget _getListWidget() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          ListView.builder(
+            itemCount: 5,
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(137, 221, 217, 217),
+                ),
+                height: 150,
+                margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              );
+            },
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabNotifier.dispose();
+    _tabController.removeListener(_listener);
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
@@ -25,14 +103,68 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
-        body: DefaultTabController(
-          length: 4,
-          child: Column(
-            children: [
-              _header(),
-              
-            ],
-          ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 10),
+            _header(),
+            const SizedBox(height: 10),
+            TabBar(
+              isScrollable: true,
+              controller: _tabController,
+              indicatorColor: Colors.orangeAccent,
+              indicatorSize: TabBarIndicatorSize.label,
+              labelColor: Colors.white,
+              labelStyle: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+              unselectedLabelStyle: const TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
+                fontSize: 25,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              labelPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+              dividerColor: Colors.black,
+              tabs: const [
+                Tab(text: "BreakFast"),
+                Tab(text: "Lunch"),
+                Tab(text: "Snacks"),
+                Tab(text: "Dinner"),
+              ],
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  ColoredBox(
+                    color: Colors.white,
+                    child: TabBarView(
+                      controller: _tabController,
+                      children: [for (int i = 0; i < _items.length; i++) _getListWidget()],
+                    ),
+                  ),
+                  ValueListenableBuilder(
+                    valueListenable: _tabNotifier,
+                    builder: (context, value, _) {
+                      return AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 300),
+                        transitionBuilder: (child, animation) => FadeTransition(
+                          opacity: animation,
+                          child: child,
+                        ),
+                        child: Image.network(
+                          imageLinks[value],
+                          key: ValueKey<int>(_tabNotifier.value),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -54,5 +186,14 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         child,
       ],
     );
+  }
+}
+
+class _TabState extends InheritedWidget {
+  _TabState({required super.child});
+
+  @override
+  bool updateShouldNotify(covariant _TabState oldWidget) {
+    return false;
   }
 }

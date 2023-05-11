9070cae28be0ea8f334e447debcbcc65c47b4a6a
cupcake08 <bhankhariaa@gmail.com>
2023-05-11 19:01:35 +0530

    :tada: rolly roll defined

--- rolly_ball/lib/custom_painters.dart
+++ rolly_ball/lib/custom_painters.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math show pi, min, cos, sin, atan2;
-
-import 'package:rolly_ball/extensions.dart';
+import 'dart:math' as math show pi;
 
 class OuterCircle extends CustomPainter {
   final Paint _paint, _innerPaint;
   final Paint _strokePaint;
-  final double shift;
+  final ValueNotifier<double> shift;
 
   OuterCircle(this.shift)
       : _paint = Paint()
@@ -19,25 +17,8 @@ class OuterCircle extends CustomPainter {
           ..color = Colors.purpleAccent
           ..style = PaintingStyle.stroke
           ..strokeCap = StrokeCap.round
-          ..strokeWidth = 30;
-
-  List<Offset> calculateTrianglePoints(Offset center, Offset target) {
-    const double radius = 15;
-    final double angle = math.atan2(target.dy - center.dy, target.dx - center.dx);
-    final double x1 = center.dx + radius * math.cos(angle - math.pi / 6);
-    final double y1 = center.dy + radius * math.sin(angle - math.pi / 6);
-    final double x2 = center.dx + radius * math.cos(angle + math.pi / 6);
-    final double y2 = center.dy + radius * math.sin(angle + math.pi / 6);
-
-    return [
-      center,
-      Offset(x1, y1),
-      Offset(x2, y2),
-      // Offset(x1, y1),
-      // center,
-      // Offset(x2, y2),
-    ];
-  }
+          ..strokeWidth = 30,
+        super(repaint: shift);
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -56,70 +37,17 @@ class OuterCircle extends CustomPainter {
 
     final metrics = path.computeMetrics().first;
     final length = metrics.length;
-    final wLen = length * .4;
+    final wLen = length * shift.value;
     final tangent = metrics.getTangentForOffset(wLen);
     final point = tangent!.position;
-    point.log();
     canvas.drawPath(path, _paint);
     final p = metrics.extractPath(0, wLen);
     canvas.drawPath(p, _strokePaint);
     _innerPaint.color = Colors.white;
     canvas.drawCircle(point, _paint.strokeWidth / 2, _innerPaint);
-
-    // _paint.color = Colors.purpleAccent;
-    // _paint.strokeWidth = 5;
-    // final sw = _paint.strokeWidth;
-    // final a = (sw + 30) * math.cos(tangent.angle);
-    // final b = (sw + 30) * math.sin(tangent.angle);
-    // final workPoint = point + Offset(a, b);
-    // workPoint.log();
-    // final offset = Offset(point.dx + 15, point.dy + 30);
-    // final List<Offset> points = calculateTrianglePoints(point, workPoint);
-    // // final List<Offset> points = [offset];
-    // canvas.drawPoints(
-    //   PointMode.points,
-    //   points,
-    //   _paint,
-    // );
     canvas.restore();
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
-}
-
-class InnerCircle extends CustomPainter {
-  final Paint _paint;
-  InnerCircle() : _paint = Paint()..color = Colors.grey;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    canvas.save();
-
-    final w = size.width;
-    final h = size.height;
-    final majorRadius = math.min(w, h) / 2 * .5;
-    final center = Offset(w / 2, h / 2);
-    final radius = math.min(w, h) * .5;
-    final rect = Rect.fromCircle(center: center, radius: radius);
-    final path = Path();
-    path.addOval(rect);
-    canvas.drawShadow(path, Colors.white, 10, false);
-    canvas.drawPath(path, _paint);
-    _paint.color = Colors.black;
-    canvas.drawCircle(center, radius * .2, _paint);
-    _paint
-      ..color = Colors.purpleAccent
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 3;
-    canvas.drawCircle(center, radius / 3, _paint);
-    canvas.restore();
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
--- rolly_ball/lib/main.dart
+++ rolly_ball/lib/main.dart
@@ -36,6 +36,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final ScrollController _scrollController = ScrollController();
+  final ValueNotifier<double> shift = ValueNotifier(0.0);
 
   @override
   void initState() {
@@ -45,9 +46,8 @@ class _HomeState extends State<Home> {
 
   _listener() {
     if (_scrollController.hasClients) {
-      _scrollController.position.pixels.log();
-      _scrollController.position.maxScrollExtent.log();
-      if (_scrollController.position.pixels > 2000.0) {}
+      final percent = _scrollController.position.pixels / _scrollController.position.maxScrollExtent;
+      shift.value = percent.clamp(0.0, 1.0);
     }
   }
 
@@ -64,82 +64,32 @@ class _HomeState extends State<Home> {
       alignment: Alignment.center,
       children: [
         CustomPaint(
-          painter: OuterCircle(.3),
+          painter: OuterCircle(shift),
           size: context.screenSize,
         ),
         SizedBox(
           height: context.height / 3,
           width: context.width / 2,
-          child: ListWheelScrollView.useDelegate(
-            controller: _scrollController,
-            itemExtent: 50,
-            squeeze: 1.2,
-            diameterRatio: .8,
-            childDelegate: ListWheelInfiniteDelegate(
-              children: [
-                Container(
+          child: ClipPath(
+            clipper: ShapeBorderClipper(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(context.height / 2))),
+            child: ListWheelScrollView.useDelegate(
+              controller: _scrollController,
+              itemExtent: 50,
+              squeeze: 1.2,
+              diameterRatio: .8,
+              physics: const BouncingScrollPhysics(),
+              childDelegate: ListWheelInfiniteDelegate(
+                childCount: 15,
+                child: Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(context.width / 2),
                     color: Colors.red,
                   ),
                 ),
-              ],
+              ),
             ),
-            // childDelegate: ListWheelChildBuilderDelegate(
-            //   childCount: 10,
-            //   builder: (context, index) {
-            //     return Container(
-            //       decoration: BoxDecoration(
-            //         borderRadius: BorderRadius.circular(context.width / 2),
-            //         color: Colors.red,
-            //       ),
-            //     );
-            //   },
-            // ),
-            // childDelegate: ListWheelChildLoopingListDelegate(
-            //   children: [
-            //     Container(
-            //       decoration: BoxDecoration(
-            //         borderRadius: BorderRadius.circular(context.width / 2),
-            //         color: Colors.red,
-            //       ),
-            //     ),
-            //     // for (int i = 0; i < 10; i++)
-            //     //   Container(
-            //     //     decoration: BoxDecoration(
-            //     //       borderRadius: BorderRadius.circular(context.width / 2),
-            //     //       color: Colors.red,
-            //     //     ),
-            //     //   )
-            //   ],
-            // ),
-            // children: [
-            //   for (int i = 0; i < 10; i++)
-            //     Container(
-            //       decoration: BoxDecoration(
-            //         borderRadius: BorderRadius.circular(context.width / 2),
-            //         color: Colors.red,
-            //       ),
-            //       margin: const EdgeInsets.symmetric(vertical: 5),
-            //     )
-            // ],
           ),
         ),
-        // MouseRegion(
-        //   // onHorizontalDragStart: (details) {
-        //   //   "Horizontal start".log();
-        //   // },
-        //   // onVerticalDragStart: (details) {
-        //   //   "vertical".log();
-        //   // },
-        //   onEnter: (event) {
-        //     "enter".log();
-        //   },
-        //   child: CustomPaint(
-        //     foregroundPainter: InnerCircle(),
-        //     size: context.screenSize / 2,
-        //   ),
-        // ),
       ],
     );
     // return CustomPaint(
@@ -152,33 +102,27 @@ class _HomeState extends State<Home> {
 
 class ListWheelInfiniteDelegate extends ListWheelChildDelegate {
   /// Constructs the delegate from a builder callback.
-  ListWheelInfiniteDelegate({required this.children});
+  ListWheelInfiniteDelegate({
+    required this.child,
+    required this.childCount,
+  });
 
-  final List<Widget> children;
-
-  /// {@template flutter.widgets.ListWheelChildBuilderDelegate.childCount}
-  /// If non-null, [childCount] is the maximum number of children that can be
-  /// provided, and children are available from 0 to [childCount] - 1.
-  ///
-  /// If null, then the lower and upper limit are not known. However the [builder]
-  /// must provide children for a contiguous segment. If the builder returns null
-  /// at some index, the segment terminates there.
-  /// {@endtemplate}
-  final int childCount = 1;
+  final int childCount;
+  final Widget child;
 
   @override
-  int? get estimatedChildCount => null;
-
+  int? get estimatedChildCount => childCount;
 
   @override
   Widget? build(BuildContext context, int index) {
-    return IndexedSemantics(index: index, child: children[index % childCount]);
+    return IndexedSemantics(index: index, child: child);
   }
 
   @override
   bool shouldRebuild(covariant ListWheelInfiniteDelegate oldDelegate) {
-    return children.first != oldDelegate.children.first;
+    return child != oldDelegate.child || childCount != oldDelegate.childCount;
   }
 }
 
+// class CircleClipper extends CustomClipper
 // class CircleClipper extends CliP

788c9f40d0b4fb8a5421aba2aec85bd29c084f31
cupcake08 <bhankhariaa@gmail.com>
2023-05-11 18:40:30 +0530

    :tada: rolly roll defined

--- rolly_ball/lib/main.dart
+++ rolly_ball/lib/main.dart
@@ -35,6 +35,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_listener);
+  }
+
+  _listener() {
+    if (_scrollController.hasClients) {
+      _scrollController.position.pixels.log();
+      _scrollController.position.maxScrollExtent.log();
+      if (_scrollController.position.pixels > 2000.0) {}
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_listener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -44,21 +67,79 @@ class _HomeState extends State<Home> {
           painter: OuterCircle(.3),
           size: context.screenSize,
         ),
-        MouseRegion(
-          // onHorizontalDragStart: (details) {
-          //   "Horizontal start".log();
-          // },
-          // onVerticalDragStart: (details) {
-          //   "vertical".log();
-          // },
-          onEnter: (event) {
-            "enter".log();
-          },
-          child: CustomPaint(
-            foregroundPainter: InnerCircle(),
-            size: context.screenSize / 2,
+        SizedBox(
+          height: context.height / 3,
+          width: context.width / 2,
+          child: ListWheelScrollView.useDelegate(
+            controller: _scrollController,
+            itemExtent: 50,
+            squeeze: 1.2,
+            diameterRatio: .8,
+            childDelegate: ListWheelInfiniteDelegate(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(context.width / 2),
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            // childDelegate: ListWheelChildBuilderDelegate(
+            //   childCount: 10,
+            //   builder: (context, index) {
+            //     return Container(
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(context.width / 2),
+            //         color: Colors.red,
+            //       ),
+            //     );
+            //   },
+            // ),
+            // childDelegate: ListWheelChildLoopingListDelegate(
+            //   children: [
+            //     Container(
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(context.width / 2),
+            //         color: Colors.red,
+            //       ),
+            //     ),
+            //     // for (int i = 0; i < 10; i++)
+            //     //   Container(
+            //     //     decoration: BoxDecoration(
+            //     //       borderRadius: BorderRadius.circular(context.width / 2),
+            //     //       color: Colors.red,
+            //     //     ),
+            //     //   )
+            //   ],
+            // ),
+            // children: [
+            //   for (int i = 0; i < 10; i++)
+            //     Container(
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(context.width / 2),
+            //         color: Colors.red,
+            //       ),
+            //       margin: const EdgeInsets.symmetric(vertical: 5),
+            //     )
+            // ],
           ),
         ),
+        // MouseRegion(
+        //   // onHorizontalDragStart: (details) {
+        //   //   "Horizontal start".log();
+        //   // },
+        //   // onVerticalDragStart: (details) {
+        //   //   "vertical".log();
+        //   // },
+        //   onEnter: (event) {
+        //     "enter".log();
+        //   },
+        //   child: CustomPaint(
+        //     foregroundPainter: InnerCircle(),
+        //     size: context.screenSize / 2,
+        //   ),
+        // ),
       ],
     );
     // return CustomPaint(
@@ -68,3 +149,36 @@ class _HomeState extends State<Home> {
     // );
   }
 }
+
+class ListWheelInfiniteDelegate extends ListWheelChildDelegate {
+  /// Constructs the delegate from a builder callback.
+  ListWheelInfiniteDelegate({required this.children});
+
+  final List<Widget> children;
+
+  /// {@template flutter.widgets.ListWheelChildBuilderDelegate.childCount}
+  /// If non-null, [childCount] is the maximum number of children that can be
+  /// provided, and children are available from 0 to [childCount] - 1.
+  ///
+  /// If null, then the lower and upper limit are not known. However the [builder]
+  /// must provide children for a contiguous segment. If the builder returns null
+  /// at some index, the segment terminates there.
+  /// {@endtemplate}
+  final int childCount = 1;
+
+  @override
+  int? get estimatedChildCount => null;
+
+
+  @override
+  Widget? build(BuildContext context, int index) {
+    return IndexedSemantics(index: index, child: children[index % childCount]);
+  }
+
+  @override
+  bool shouldRebuild(covariant ListWheelInfiniteDelegate oldDelegate) {
+    return children.first != oldDelegate.children.first;
+  }
+}
+
+// class CircleClipper extends CliP
